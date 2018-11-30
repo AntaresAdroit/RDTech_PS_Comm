89903f49616c7fb189df761c8bbe66bb67e33afc
AntaresAdroit <AntaresAdroit@gmail.com>
2018-11-30 12:03:28 -0800

    Add files via upload
    Adding a proper library example that demonstrates all of the implemented functions.

---
+++ examples/BasicTest/BasicTest.ino
@@ -0,0 +1,40 @@
+//Tested on a Pololu A-Star 32U4 board which used the ATmega 32U4 Microcontroller.
+//Serial1 is the hardware serial port on the board and utilizes digital pins 0 and 1.
+
+#include <Stream.h>
+#include <RCTech_PS_Comm.h>
+
+DPS_powerSupply supply1;
+
+void setup() {
+  Serial1.begin(19200);
+  //Serial.begin(57600);
+  delay(1000);
+
+  supply1.initDPS(Serial1, 0x01);
+}
+
+void loop() {
+
+  supply1.setDPS_voltage(600);
+  delay(500);
+  supply1.setDPS_amperage(400);
+  delay(500);
+  supply1.setDPS_outputOn(true);
+  delay(7000);
+  supply1.setDPS_voltage(900);
+  delay(7000);
+  supply1.setDPS_outputOn(false);
+  delay(500);
+  supply1.setDPS_voltageamperage(600, 1000);
+  delay(500);
+  supply1.setDPS_outputOn(true);
+  delay(7000);
+  supply1.setDPS_outputOn(false);
+  delay(7000);
+
+}
+
+
+
+
